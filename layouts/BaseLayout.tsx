4e57a6b2e68a3d4d5f574837258032e34d2401cc
Somil Mathur <somil@propreturns.com>
2022-11-22 19:02:54 +0530

    changed to px-4

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -8,7 +8,7 @@ type Props = any;
 function BaseLayout({ children }: Props) {
 	return (
 		<div className="mx-auto">
-			<div className="mx-auto container px-6">
+			<div className="mx-auto px-4">
 				<Navbar />
 				<Search />
 				<hr className="w-10/12 opacity-50 mt-4" />
