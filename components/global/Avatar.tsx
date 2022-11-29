93a8e80a668afec92717ce1244c12a8951088c62
Somil Mathur <somil@propreturns.com>
2022-11-29 17:41:22 +0530

    testing the bot

--- components/global/Avatar.tsx
+++ components/global/Avatar.tsx
@@ -10,7 +10,7 @@ function Avatar({height, width}: Props) {
 		<div className="flex -space-x-2 overflow-hidden mr-4 relative">
 			<img
 				className={`inline-block ${height || "h-14"} ${width || "w-14"} rounded-full ring-2 ring-white`}
-				src="/img/avatar.jpg"
+				src="/img/avatar.jpeg"
 				alt="Sanjana and Arjun avatar"
 			/>
 			<div className="absolute bottom-0.5 right-0.5 w-3 h-3 rounded-full bg-green-500" />
