40e954e44be7c9e70f0378718f9dc2779534370b
Somil Mathur <somil@propreturns.com>
2022-12-10 19:15:44 +0530

    All message stuff done

--- components/layout-components/BottomNavBar.tsx
+++ components/layout-components/BottomNavBar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { FiMapPin, FiMessageCircle, FiPlus, FiEdit2 } from "react-icons/fi";
+import { useAppContext } from "../../context/app/appContext";
 import Avatar from "../global/Avatar";
 import Button from "../global/buttons/Button";
 import MotionDrawer from "../global/MotionDrawer";
@@ -10,9 +11,25 @@ type Props = {};
 
 function BottomNavBar({}: Props) {
 	const router = useRouter();
-	console.log(router)
+	const { UUID, Loading, SetLoading } = useAppContext();
 	const [showSendMessageDrawer, setShowSendMessageDrawer] = useState(false);
 	const activePage = router.pathname === "/" ? "home" : "location";
+	const [message, setMessage] = useState("");
+	const [ThankyouScreen, setThankyouScreen] = useState(false);
+
+	const handleSendMessage = async () => {
+		SetLoading(true);
+		await fetch("/api/sendMessage", {
+			method: "POST",
+			body: JSON.stringify({
+				message,
+				UUID,
+			}),
+		});
+		SetLoading(false);
+		setThankyouScreen(true)
+	};
+
 	return (
 		<div className="h-20 w-full px-10 items-center fixed bottom-0 flex justify-around items-center shadow-custom-card rounded-t-3xl">
 			<Link href={{ pathname: "/", query: router.query }}>
@@ -42,26 +59,50 @@ function BottomNavBar({}: Props) {
 				setShow={setShowSendMessageDrawer}
 			>
 				<div className="py-10 px-4">
-					{/* To box */}
-					<div className="bg-gray-50 my-6 rounded-xl p-4 my-2 flex items-center">
-						<span className="text-sm font-bold text-gray-400">
-							To: <span className="text-gray-700 mr-2">Sanjana and Arjun</span>
-						</span>
-						<Avatar height="h-8 w-8" hideOnlineIndicator />
-					</div>
+					{/* Message screen  */}
+					{!ThankyouScreen && (
+						<div>
+							{/* To box */}
+							<div className="bg-gray-50 my-6 rounded-xl p-4 my-2 flex items-center">
+								<span className="text-sm font-bold text-gray-400">
+									To:{" "}
+									<span className="text-gray-700 mr-2">Sanjana and Arjun</span>
+								</span>
+								<Avatar height="h-8 w-8" hideOnlineIndicator />
+							</div>
 
-					{/* Text editor  */}
-					<div className="my-4">
-						<textarea
-							id="message"
-							rows={4}
-							className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-xl focus:ring-gray-300 focus:border-blue-500"
-							placeholder="Send us a message to let us know how excited you are!"
-						/>
-					</div>
+							{/* Text editor  */}
+							<div className="my-4">
+								<textarea
+									id="message"
+									rows={4}
+									className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-xl focus:ring-gray-300 focus:border-blue-500"
+									placeholder="Send us a message to let us know how excited you are!"
+									onChange={(message) => setMessage(message.target.value)}
+								/>
+							</div>
 
-					{/* Submit button */}
-					<Button appearance="Primary">Send message</Button>
+							{/* Submit button */}
+							<Button
+								appearance="Primary"
+								loading={Loading}
+								onClick={() => handleSendMessage()}
+							>
+								Send message
+							</Button>
+						</div>
+					)}
+
+					{ThankyouScreen && (
+						<div>
+							<p className="text-gray-900 font-bold text-center text-2xl text-center">
+								✌️
+							</p>
+							<p className="text-gray-900 font-bold text-center text-md text-center mt-4">
+								Thanks for your message! Hope to see you very soon!
+							</p>
+						</div>
+					)}
 				</div>
 			</MotionDrawer>
 		</div>
