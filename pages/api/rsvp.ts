afd4ed784be5abf348ada28f3c673928805f9128
Somil Mathur <somil@propreturns.com>
2022-12-10 17:24:39 +0530

    Added set in API

--- pages/api/rsvp.ts
+++ pages/api/rsvp.ts
@@ -9,19 +9,21 @@ export default async function handler(req, res) {
 	switch (req.method) {
 		case "POST":
 			let bodyObject = JSON.parse(req.body);
-			console.log("body", bodyObject);		
-			let rsvp = await db.collection("rsvp").updateOne(
+			console.log("body", bodyObject);
+			let rsvp = await db.collection("rsvp").update(
 				{ UUID: bodyObject.UUID },
 				{
-					name: bodyObject.answers.steps["rsvp-answer"]?.message,
-					requiresAccommodation:
-						bodyObject.answers.steps["accomodation-answer"]?.message,
-					peopleInParty:
-						bodyObject.answers.steps["how-many-people-answer"].value,
-					createdAt: dayjs()
-						.utcOffset(330)
-						.format(),
-					answers: bodyObject.answers.steps,
+					$set: {
+						name: bodyObject.answers.steps["rsvp-answer"]?.message,
+						requiresAccommodation:
+							bodyObject.answers.steps["accomodation-answer"]?.message,
+						peopleInParty:
+							bodyObject.answers.steps["how-many-people-answer"].value,
+						createdAt: dayjs()
+							.utcOffset(330)
+							.format(),
+						answers: bodyObject.answers.steps,
+					},
 				},
 				{ upsert: true }
 			);
