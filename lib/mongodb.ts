a2c141fea74f7f6b0dc04300cfb201820b1b4998
Somil Mathur <somil@propreturns.com>
2022-12-10 13:00:18 +0530

    rebuilding change

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,10 +3,6 @@
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-}
 
 let client
 let clientPromise
