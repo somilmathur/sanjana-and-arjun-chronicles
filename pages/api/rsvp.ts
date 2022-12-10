9ae1e9328d6277551f0ae73392247345c44ae766
Somil Mathur <somil@propreturns.com>
2022-12-10 17:20:25 +0530

    Fixed rsvp API

--- pages/api/rsvp.ts
+++ pages/api/rsvp.ts
@@ -9,21 +9,22 @@ export default async function handler(req, res) {
 	switch (req.method) {
 		case "POST":
 			let bodyObject = JSON.parse(req.body);
-			console.log("body", bodyObject);
-			const rsvpObject = {
-				UUID: bodyObject.UUID,
-				name: bodyObject.answers.steps["rsvp-answer"]?.message,
-				requiresAccommodation:
-					bodyObject.answers.steps["accomodation-answer"]?.message,
-				peopleInParty: bodyObject.answers.steps["how-many-people-answer"].value,
-				createdAt: dayjs()
-					.utcOffset(330)
-					.format(),
-				answers: bodyObject.answers.steps,
-			};
-			let rsvp = await db
-				.collection("rsvp")
-				.updateOne({ UUID: rsvpObject.UUID }, rsvpObject, { upsert: true });
+			console.log("body", bodyObject);		
+			let rsvp = await db.collection("rsvp").updateOne(
+				{ UUID: bodyObject.UUID },
+				{
+					name: bodyObject.answers.steps["rsvp-answer"]?.message,
+					requiresAccommodation:
+						bodyObject.answers.steps["accomodation-answer"]?.message,
+					peopleInParty:
+						bodyObject.answers.steps["how-many-people-answer"].value,
+					createdAt: dayjs()
+						.utcOffset(330)
+						.format(),
+					answers: bodyObject.answers.steps,
+				},
+				{ upsert: true }
+			);
 			console.log("bodyObject ->>", rsvp);
 			res.json(rsvp);
 			break;
