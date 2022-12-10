4d705c2cf5027335624eeb96283956b18bd13575
Somil Mathur <somil@propreturns.com>
2022-12-10 14:15:50 +0530

    API completed for 1 case

--- pages/api/rsvp.ts
+++ pages/api/rsvp.ts
@@ -1,4 +1,7 @@
 import clientPromise from "../../lib/mongodb";
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
 
 export default async function handler(req, res) {
   const client = await clientPromise;
@@ -6,7 +9,14 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       let bodyObject = JSON.parse(req.body);
-      let rsvp = await db.collection("rsvp").insertOne(bodyObject);
+      const rsvpObject = {
+        name: bodyObject.steps["rsvp-answer"].message,
+        requiresAccommodation: bodyObject.steps["accomodation-answer"]?.message,
+        peopleInParty: bodyObject.steps["how-many-people-answer"].message,
+        createdAt: dayjs().utcOffset(330).format(),
+        answers: bodyObject.steps,
+      }
+      let rsvp = await db.collection("rsvp").insertOne(rsvpObject);
       console.log("bodyObject ->>", rsvp)
       res.json(rsvp);
       break;   
